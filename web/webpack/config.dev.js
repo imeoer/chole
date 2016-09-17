0d6bca60d7783111154671b41c6805339320b2e7
imeoer <imeoer@gmail.com>
2016-09-17 21:34:57 +0800

    add html plugin for webpack

--- web/webpack/config.dev.js
+++ web/webpack/config.dev.js
@@ -1,8 +1,20 @@
+var webpack = require('webpack');
 var baseConfig = require('./config.base');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = Object.assign({}, baseConfig, {
   output: {
     path: __dirname,
     filename: 'build.js'
-  }
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      inject: true
+    })
+  ]
 });
--- web/webpack/config.prod.js
+++ web/webpack/config.prod.js
@@ -1,11 +1,13 @@
+var path = require('path');
 var webpack = require('webpack');
 var baseConfig = require('./config.base');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = Object.assign({}, baseConfig, {
   output: {
-    path: __dirname,
-    filename: '../dist/build.js'
+    path: 'dist',
+    filename: 'build.js'
   },
   vue: {
     loaders: {
@@ -13,7 +15,7 @@ module.exports = Object.assign({}, baseConfig, {
     }
   },
   plugins: [
-    new ExtractTextPlugin("../dist/style.css"),
+    new ExtractTextPlugin("style.css"),
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: '"production"'
@@ -24,6 +26,17 @@ module.exports = Object.assign({}, baseConfig, {
         warnings: false
       }
     }),
-    new webpack.optimize.OccurenceOrderPlugin()
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      filename: 'index.html',
+      inject: true,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true
+      },
+      chunksSortMode: 'dependency'
+    })
   ]
 });
